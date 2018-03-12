02ec413db7149a77988b6ddf1f5d0ef2a52b0138
Ruben Bustos <ruben_b_r_@hotmail.com>
2018-03-12 20:00:11 +0100

    implement add()

--- src/dqueue_pointers.adb
+++ src/dqueue_pointers.adb
@@ -3,14 +3,37 @@ package body dqueue_pointers is
    procedure empty(q: out queue) is
    begin
       
-      q.first := q.last := null;
+      q.first := null; q.last := null;
       
    end empty;
    
    procedure add(q: in out queue; x: in elem) is
+      
+      new_cell: cell;
+      
    begin
       
+      new_cell = new cell'(x, null);
+      
+      q.last.next := new_cell;
+      q.last := new_cell;
+      
    end add;
+   
+   procedure delete_first(q: in out queue) is
+   begin
+      null;
+   end delete_first;
+   
+   function get_first(q: in queue) return elem is
+   begin
+      return null;
+   end get_first;
+   
+   function is_empty(q: in queue) return boolean is
+   begin
+      return null;
+   end is_empty;
    
    
 end dqueue_pointers;
