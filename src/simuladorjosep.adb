961730763ac018b527a14b988ef9be7904102650
Ruben Bustos <ruben_b_r_@hotmail.com>
2018-03-15 18:21:42 +0100

    some structure refactor

--- src/simuladorjosep.adb
+++ src/simuladorjosep.adb
@@ -8,7 +8,9 @@ procedure Simuladorjosep is
 
    --We will operate with players
    type Player is record
+
       Name: Unbounded_String;
+
    end record;
 
    package PlayersQueue is new dqueue_pointers(elem => Player); use PlayersQueue;
@@ -16,32 +18,50 @@ procedure Simuladorjosep is
    passades: Positive;
    pla_file: File_Type;
    pla_queue: queue;
+   winner: Player;
 
-   --READ A POSITIVE FROM KEYBOARD
+   -------PRIVATE METHODS---------
+
+   --READ A POSITIVE INTEGER FROM KEYBOARD
    procedure readInt(pass: out Positive) is
    begin
+
       pass := Integer'Value(Get_Line);
 
    exception
+
       when Constraint_Error => Put_Line("'Passades' must be greater than 0");
+
    end readInt;
 
-   --CREATES A QUEUE FROM A TEXT FILE THAT CONTAINS PLAYERS
+   --CREATES A QUEUE FROM A TEXT FILE THAT CONTAINS PLAYER NAMES
    procedure createQueueFromFile(q: out queue; file: in File_Type) is
+
       new_player: Player;
+
    begin
+
       empty(q);
+
       while not End_Of_File(file) loop
          Get_Line(file, new_player.Name);
          add(q, new_player);
       end loop;
+
+   exception
+
+         when overflow => Put_Line("ERROR: MAIN MEMORY OVERFLOW");
+
    end createQueueFromFile;
 
    --LET'S PLAY ;)
-   procedure murderPlayers(q: in out queue; passades: in Positive) is
+   procedure murderPlayers(q: in out queue; passades: in Positive; winner: out Player) is
+
       i: Natural := 0;
       pla: Player;
+
    begin
+
       while not is_last_item(q) and i <= passades loop
          pla := get_first(q);
          add(q, pla);
@@ -49,19 +69,28 @@ procedure Simuladorjosep is
          i := i + 1;
       end loop;
 
-      Put_Line(get_first(q).Name);
+      winner := get_first(q);
+
+   exception
+
+      when overflow => Put_Line("ERROR: MAIN MEMORY OVERFLOW");
+      when bad_use => Put_Line("QUEUE 'q' IT'S EMPTY");
+
    end murderPlayers;
+
+   ---------MAIN----------
 
 begin
 
    Open(pla_file, In_File, "Jugadors.txt");
+   createQueueFromFile(pla_queue, pla_file);
 
    Put("Passades: ");
    readInt(passades);
 
-   createQueueFromFile(pla_queue, pla_file);
+   murderPlayers(pla_queue, passades, winner);
 
-   murderPlayers(pla_queue, passades);
+   Put_Line("Guanyador/a: " & winner.Name);
 
 end Simuladorjosep;
 
